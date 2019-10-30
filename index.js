99868bcbe58a349c291d885ad7cdf55936491f2a
ale <alemarchena@gmail.com>
2019-10-30 01:33:26 -0300

    Correccion cors

--- index.js
+++ index.js
@@ -37,9 +37,6 @@ app.use('/',rutas);
 const cors = require('cors');
 app.use(cors);
 
-const cors = require('cors');
-app.use(cors());
-
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
