10e9244493b214e7984284689df2f87241f3c4c6
ale <alemarchena@gmail.com>
2019-10-29 22:42:54 -0300

    Verificando mutantes
    implementacion completa de la verificacion de mutantes, pruebas post

--- server/controllers/mutant.controller.js
+++ server/controllers/mutant.controller.js
@@ -1,7 +1,8 @@
 const controller = {};
 const mutante = require('../models/mutant.model');
 
-//creo una estructura para responder
+var esmutante = false;
+
 let respuesta = {
     error: false,
     codigo: 200,
@@ -21,36 +22,122 @@ controller.isMutant = async(req,res) => {
         }
         res.status(403).send(respuesta);
     }
-    else {
-        //creo una matriz de dos dimensiones que va a guardar los caracteres
-        var matrizCaracteres = new Array(2);
+    else 
+    {
+        //------------------------------------ MODELADO DEL JSON  -----------------------------
 
-        //convierto el json que viene del navegador a un objeto
-        let objeto = JSON.parse(mutante.dna);
-        
-        //desarmo el json para armar la matriz de caracteres
-        for (var f = 0; f < objeto.length; f++) 
+        let filas = JSON.parse(mutante.dna); //convierto el json en un objeto
+        var m = new Array(filas.length); //creo una MATRIZ de altura igual a la cantidad de filas
+        var columna;
+
+        for (var f = 0; f < filas.length; f++) //recorro las filas
         {
-            //creo un objeto por registro del json y lo llamo fila
-            let fila = objeto[f];
-            
-            //desarmo la fila de cada objeto    
-            let columna = fila.toString();
+            columna = filas[f].toString(); //guardo el CONTENIDO de la fila como una columna
+            var matrizcaracteres = new Array(columna.length); //creo una matriz para guardar los caracteres
 
             for(var c=0; c < columna.length ;c++)
-            {
+            {   //encolumno los caracteres
                 var caracter = columna.substring(c, c+1);
-                matrizCaracteres[f,c] = caracter;
+                matrizcaracteres[c] = caracter; 
             }
-            console.log(matrizCaracteres);
+            //guardo en cada fila los caracteres formando una matriz bidimensional
+            m[f] = new Array(columna.length); 
+            m[f] = columna;
+            console.log(m[f]);
         }
 
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: 'Vamos a ver si es humano o mutante'
+        //------------------------------------ VERIFICACION DE ADN -----------------------------
+        var longitudsecuencia = 4;
+        var cantidadsecuencias = 0;
+        var cl = columna.length;
+        var fl =filas.length;
+
+        //verifica secuencia HORIZONTALMENTE ----------------------------------
+        for(var f=0;f< fl ;f++)
+        {
+            var c=0;
+            while (c < (columna.length - longitudsecuencia) + 1)
+            {
+                //console.log(m[f][c] + "," + m[f][c + 1] +","+ m[f][c + 2] + "," + m[f][c + 3]);
+
+                if (m[f][c] == m[f][c + 1] && m[f][c] == m[f][c + 2] && m[f][c] == m[f][c + 3])
+                {
+                    cantidadsecuencias = cantidadsecuencias + 1;
+                    c = cl;
+                }
+                c++;
+            }
+        }//--------------------------------------------------------------------
+
+        if(cantidadsecuencias <= 1) //aun no es mutante
+        {
+            //verifica secuencia VERTICALMENTE ---------------------
+            var c = 0;
+            while(c < columna.length)
+            {
+                var f = 0;
+                while (f < (fl - longitudsecuencia) + 1) 
+                {
+                    //console.log(m[f][c] + "," + m[f + 1][c] + "," + m[f + 2][c] + "," + m[f + 3][c]);
+                    if (m[f][c] == m[f + 1][c] && m[f][c] == m[f + 2][c] && m[f][c] == m[f + 3][c]) 
+                    {
+                        cantidadsecuencias = cantidadsecuencias + 1;
+                            f = fl;
+                    }
+                    f++;
+                }
+
+                if (cantidadsecuencias > 1)
+                {
+                    esmutante = true;
+                    c = columna.length;
+                }
+                c++;
+            }//------------------------------------------------------
+
+            
+            if (cantidadsecuencias <= 1) //aun no es mutante
+            {
+                var filalimite = fl - longitudsecuencia;
+
+                //verifica en ----DIAGONAL-------------
+                for (var f = 0; f <= filalimite; f++) 
+                {
+                    var c = 0;
+                    while (c <= (cl - longitudsecuencia) )  //cuantas columnas verifica
+                    {
+                        console.log(
+                            m[f][c] + "==" + m[f + 1][c + 1] +"-"+
+                            m[f][c] + "==" + m[f + 2][c + 2] +"-"+
+                            m[f][c] + "==" + m[f + 3][c + 3]);
+                        //de izquierda a derecha 
+                        if (m[f][c] == m[f + 1][c + 1] && 
+                            m[f][c] == m[f + 2][c + 2] && 
+                            m[f][c] == m[f + 3][c + 3])
+                        {
+                            cantidadsecuencias ++;
+                        }
+
+                        //de derecha a izquierda
+                        if (m[f][cl - (1 - c)] == m[f + 1][cl - 2] && 
+                            m[f][cl - (1 - c)] == m[f + 2][cl - 3] && 
+                            m[f][cl - (1 - c)] == m[f + 3][cl - 4]) 
+                        {
+                            cantidadsecuencias ++;
+                        }
+                        c++;
+                    }
+                }//--------------------------------------
+            }else
+            {
+                esmutante = true;
+            }
+        }else{
+            esmutante = true;
         }
-        res.status(200).send(respuesta);
+        
+        
+        res.status(200).send(esmutante);
     }
 };
 
