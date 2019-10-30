fb272511de9e84632c22f3107eb0e1b0fda72481
ale <alemarchena@gmail.com>
2019-10-30 18:33:20 -0300

    Version funcionando compelta

--- server/controllers/stats.controller.js
+++ server/controllers/stats.controller.js
@@ -4,10 +4,21 @@ const estadistica = require('../models/stats.model');
 
 scontroller.getestadistica = async (req, res) => {
 
-    const est = await estadistica.find();
+    const esmut = await estadistica.find({ esmutante: true });//consulta los mutantes
+    const eshum = await estadistica.find({ esmutante: false });//consulta los humanos
 
+    let respuesta = {
+        count_mutant_dna:0,
+        count_human_dna:0,
+        ratio:0
+    };
     
-    res.json(est);
+    //calculo estadísticas
+    respuesta.count_mutant_dna = esmut.length;
+    respuesta.count_human_dna = eshum.length;
+    respuesta.ratio = esmut.length / eshum.length;
+
+    res.json(respuesta);
 };
 
 module.exports = scontroller;
