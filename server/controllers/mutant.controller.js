16f6e00e7ee9c5fab1e1b6b0401692b1049ed7ba
ale <alemarchena@gmail.com>
2019-10-31 12:30:24 -0300

    Implementando try catch

--- server/controllers/mutant.controller.js
+++ server/controllers/mutant.controller.js
@@ -1,4 +1,3 @@
-// const controller = {};
 const mutante = require('../models/mutant.model');
 const modelostats = require('../models/stats.model');
 
@@ -8,147 +7,157 @@ let respuesta = {
     mensaje: 'dna inválido'
 };
 
-exports.isMutant = async function(req, res){
-    var esmutante = false;
-    mutante.dna = req.body.dna;
+exports.isMutant = async function(req, res)
+{
+    try{
+        var esmutante = false;
+        mutante.dna = req.body.dna;
 
-    if (!mutante.dna) {
+        if (!mutante.dna) {
 
-        res.status(403).send(respuesta);
-        return;
-    }
-    else 
-    {
-        //------------------------------------ MODELADO DEL JSON  -----------------------------
-        let filas = JSON.parse(mutante.dna); //convierto el json en un objeto
-
-        var m = new Array(filas.length); //creo una MATRIZ de altura igual a la cantidad de filas
-        var columna;
-        var base = ['A', 'T', 'C', 'G'];
-        var validadorcolumna=0;
-
-        for (var f = 0; f < filas.length; f++) //recorro las filas
-        {
-            columna = filas[f].toString(); //guardo el CONTENIDO de la fila como una columna
-            var matrizcaracteres = new Array(columna.length); //creo una matriz para guardar los caracteres
-            if(f==0)
-                validadorcolumna = columna.length;
-            
-            //valido que todas los registros del json tengan el mismo tamaño
-            if(validadorcolumna != columna.length ) {res.status(403).send(respuesta);return;} //ERROR
-
-            for(var c=0; c < columna.length ;c++)
-            {   
-                var caracter = columna.substring(c, c + 1); //encolumno los caracteres
-                var basevalida=false;
-                for (veri = 0; veri < base.length; veri++) //valido que los caracteres sean de base nitrogenada
-                {
-                    if(caracter === base[veri])
-                        basevalida=true;
-                }
-
-                if (!basevalida){ res.status(403).send(respuesta); return; } //ERROR
-
-                if(caracter )
-                matrizcaracteres[c] = caracter; 
-            }
-            //guardo en cada fila los caracteres formando una matriz bidimensional
-            m[f] = new Array(columna.length); 
-            m[f] = columna;
+            res.status(403).send(respuesta);
+            return;
         }
+        else 
+        {
+            //------------------------------------ MODELADO DEL JSON  -----------------------------
+            let filas = JSON.parse(mutante.dna); //convierto el json en un objeto
 
-        //------------------------------------ VERIFICACION DE ADN -----------------------------
-        var longitudsecuencia = 4;
-        var cantidadsecuencias = 0;
-        var cl = columna.length;
-        var fl =filas.length;
-       
-        for (var f = 0; f < fl; f++) //verifica secuencia HORIZONTALMENTE --------------------
-        {
-            var c=0;
-            while (c <= (columna.length - longitudsecuencia))
+            var m = new Array(filas.length); //creo una MATRIZ de altura igual a la cantidad de filas
+            var columna;
+            var base = ['A', 'T', 'C', 'G'];
+            var validadorcolumna=0;
+
+            for (var f = 0; f < filas.length; f++) //recorro las filas
             {
-                if (m[f][c] == m[f][c + 1] && m[f][c] == m[f][c + 2] && m[f][c] == m[f][c + 3])
-                {
-                    cantidadsecuencias = cantidadsecuencias + 1;
-                    c = cl;
+                columna = filas[f].toString(); //guardo el CONTENIDO de la fila como una columna
+                var matrizcaracteres = new Array(columna.length); //creo una matriz para guardar los caracteres
+                if(f==0)
+                    validadorcolumna = columna.length;
+                
+                //valido que todas los registros del json tengan el mismo tamaño
+                if(validadorcolumna != columna.length ) {res.status(403).send(respuesta);return;} //ERROR
+
+                for(var c=0; c < columna.length ;c++)
+                {   
+                    var caracter = columna.substring(c, c + 1); //encolumno los caracteres
+                    var basevalida=false;
+                    for (veri = 0; veri < base.length; veri++) //valido que los caracteres sean de base nitrogenada
+                    {
+                        if(caracter === base[veri])
+                            basevalida=true;
+                    }
+
+                    if (!basevalida){ res.status(403).send(respuesta); return; } //ERROR
+
+                    if(caracter )
+                    matrizcaracteres[c] = caracter; 
                 }
-                c++;    
+                //guardo en cada fila los caracteres formando una matriz bidimensional
+                m[f] = new Array(columna.length); 
+                m[f] = columna;
             }
-            
-        }//----------------------------------------------------------------------------------
+
+            //------------------------------------ VERIFICACION DE ADN -----------------------------
+            var longitudsecuencia = 4;
+            var cantidadsecuencias = 0;
+            var cl = columna.length;
+            var fl =filas.length;
         
-        if(cantidadsecuencias <= 1) //si aun no es mutante
-        {
-            var c = 0;
-            while (c < columna.length)//verifica secuencia VERTICALMENTE ---------------------
+            for (var f = 0; f < fl; f++) //verifica secuencia HORIZONTALMENTE --------------------
             {
-                var f = 0;
-                while (f <= (fl - longitudsecuencia)) 
+                var c=0;
+                while (c <= (columna.length - longitudsecuencia))
                 {
-                    if (m[f][c] == m[f + 1][c] && m[f][c] == m[f + 2][c] && m[f][c] == m[f + 3][c]) 
+                    if (m[f][c] == m[f][c + 1] && m[f][c] == m[f][c + 2] && m[f][c] == m[f][c + 3])
                     {
                         cantidadsecuencias = cantidadsecuencias + 1;
-                            f = fl;
+                        c = cl;
                     }
-                    f++;
+                    c++;    
                 }
-
-                if (cantidadsecuencias > 1)
-                {
-                    esmutante = true;
-                    c = columna.length;
-                }
-                c++;
-            }//--------------------------------------------------------------------------------
+                
+            }//----------------------------------------------------------------------------------
             
-            if (cantidadsecuencias <= 1) //si aun no es mutante
+            if(cantidadsecuencias <= 1) //si aun no es mutante
             {
-                var filalimite = fl - longitudsecuencia;
-                for (var f = 0; f <= filalimite; f++)  //verifica en ----DIAGONAL-------------
+                var c = 0;
+                while (c < columna.length)//verifica secuencia VERTICALMENTE ---------------------
                 {
-                    var c = 0;
-                    while (c <= (cl - longitudsecuencia) )  //cuantas columnas verifica
+                    var f = 0;
+                    while (f <= (fl - longitudsecuencia)) 
                     {
-                        //de izquierda a derecha 
-                        if (m[f][c] == m[f + 1][c + 1] && 
-                            m[f][c] == m[f + 2][c + 2] && 
-                            m[f][c] == m[f + 3][c + 3])
+                        if (m[f][c] == m[f + 1][c] && m[f][c] == m[f + 2][c] && m[f][c] == m[f + 3][c]) 
                         {
-                            cantidadsecuencias ++;
+                            cantidadsecuencias = cantidadsecuencias + 1;
+                                f = fl;
                         }
-                        //de derecha a izquierda
-                        if (m[f][cl - (1 - c)] == m[f + 1][cl - 2] && 
-                            m[f][cl - (1 - c)] == m[f + 2][cl - 3] && 
-                            m[f][cl - (1 - c)] == m[f + 3][cl - 4]) 
-                        {
-                            cantidadsecuencias ++;
-                        }
-                        c++;
+                        f++;
                     }
-                }//----------------------------------------------------------------
+
+                    if (cantidadsecuencias > 1)
+                    {
+                        esmutante = true;
+                        c = columna.length;
+                    }
+                    c++;
+                }//--------------------------------------------------------------------------------
+                
+                if (cantidadsecuencias <= 1) //si aun no es mutante
+                {
+                    var filalimite = fl - longitudsecuencia;
+                    for (var f = 0; f <= filalimite; f++)  //verifica en ----DIAGONAL-------------
+                    {
+                        var c = 0;
+                        while (c <= (cl - longitudsecuencia) )  //cuantas columnas verifica
+                        {
+                            //de izquierda a derecha 
+                            if (m[f][c] == m[f + 1][c + 1] && 
+                                m[f][c] == m[f + 2][c + 2] && 
+                                m[f][c] == m[f + 3][c + 3])
+                            {
+                                cantidadsecuencias ++;
+                            }
+                            //de derecha a izquierda
+                            if (m[f][cl - (1 - c)] == m[f + 1][cl - 2] && 
+                                m[f][cl - (1 - c)] == m[f + 2][cl - 3] && 
+                                m[f][cl - (1 - c)] == m[f + 3][cl - 4]) 
+                            {
+                                cantidadsecuencias ++;
+                            }
+                            c++;
+                        }
+                    }//----------------------------------------------------------------
+                }
             }
-        }
-        
-        var codigostatus = 0;
-        if(cantidadsecuencias <=1) //¿Es mutante o no ?
-        {
-            esmutante = false;
-            codigostatus = 403;
-        }else{
-            esmutante = true;
-            codigostatus = 200;
-        }
+            
+            var codigostatus = 0;
+            if(cantidadsecuencias <=1) //¿Es mutante o no ?
+            {
+                esmutante = false;
+                codigostatus = 403;
+            }else{
+                esmutante = true;
+                codigostatus = 200;
+            }
 
-        //guardo el registro en bdd
-        const estadistica = new modelostats();
-        estadistica.esmutante = esmutante;
-        estadistica.dna = mutante.dna;
-        await estadistica.save();
+            //guardo el registro en bdd
+            const estadistica = new modelostats();
+            estadistica.esmutante = esmutante;
+            estadistica.dna = mutante.dna;
+            await estadistica.save();
 
-        //respondo
-        res.status(codigostatus).send(esmutante);
+            //respondo
+            res.status(codigostatus).send(esmutante);
+        }
+    }catch(e)
+    {
+        respuesta = {
+            error: true,
+            codigo: 401,
+            mensaje: 'Se produjo un error en el verificador'
+        };
+        codigostatus = 401;
+        res.status(codigostatus).send(respuesta);
     }
 };
-
-//  module.exports = controller;
--- test/mutant.controller-test.js
+++ test/mutant.controller-test.js
@@ -4,22 +4,173 @@ const expect = require('chai').expect;
 chai.use(chaiHttp);
 const url = "https://mutant-ml.herokuapp.com";
 var path = '/mutant';
+var epath = '/stats';
 
-describe('Controlador de mutantes ', () => {
-    console.log (url);
-    it('Verificamos si es mutante o humano', (done) => {
+describe('Controlador de mutantes', () => {
+    it('Petición POST vacía', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({})
+            .end(function (err, res, body) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(1000);
+
+    it('¿ es HUMANO ? - Matriz adn CUADRADA 6x6', (done) => {
         chai.request(url)
             .post(path)         
             .set('Content-Type', 'application/json')
             .send({ "dna": "[\"AATAGA\",\"CAGTGC\",\"TTAGTT\",\"AGACGG\",\"GCGTCA\",\"TCGGGG\"]" })
-            .end(function (err, res,body) {
+            .end(function (err, res) {
                 if(err){
                     done(err);
                 }else{
                     expect(res).to.have.status(403); 
-                    
                     done();
+                   
                 }  
+            });
+    }).timeout(10000);
+
+    it('¿ es MUTANTE ? - Matriz adn CUADRADA 6x6', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AAAAAT\",\"CAGTGC\",\"TTAGTT\",\"AGACGG\",\"GCGTCA\",\"TCGGGG\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(200);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('DNA erróneo - Matriz CUADRADA 6x6', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AATGA\",\"CAGTGC\",\"TTAGTT\",\"AGACGG\",\"GCGTCA\",\"TCGGGG\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    //matriz 4x6
+    it('¿ es HUMANO ? - Matriz adn 4x6', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AATAGA\",\"CAGTGC\",\"TTAGTT\",\"AGACGG\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('¿ es MUTANTE ? - Matriz adn 4x6', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AAAAAT\",\"CAGTGC\",\"GCGTCA\",\"TCGGGG\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(200);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('DNA erróneo - Matriz 4x6', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AATGA\",\"CAGTGC\",\"GCGTCA\",\"TCGGGG\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    //matriz 8x5
+    it('¿ es HUMANO ? - Matriz adn 8x5 - BUSQUEDA DIAGONAL DER-IZQ , IZQ-DER', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"CCTAA\",\"AGTGC\",\"TAGTT\",\"AGACG\",\"TTGGT\",\"TATAT\",\"TTAGT\",\"CTGAC\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('¿ es MUTANTE ? - Matriz adn 8x5', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AATAGA\",\"CAGTGC\",\"TTAGTT\",\"AGAAGG\",\"TTTGGT\",\"TTATAT\",\"TTAGTT\",\"TTAGAT\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(200);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('DNA erróneo - Matriz 8x5 caracter inválido en dna', (done) => {
+        chai.request(url)
+            .post(path)
+            .set('Content-Type', 'application/json')
+            .send({ "dna": "[\"AATAGA\",\"CAGTGC\",\"TTyGTT\",\"AGAAGG\",\"TTTGGT\",\"TTATAT\",\"TTAGTT\",\"TTAGAT\"]" })
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(403);
+                    done();
+                }
+            });
+    }).timeout(10000);
+
+    it('Get Estadísticas ', (done) => {
+        chai.request(url)
+            .get(epath)
+            .end(function (err, res) {
+                if (err) {
+                    done(err);
+                } else {
+                    expect(res).to.have.status(200);
+                    done();
+                    console.log(res.body);
+                }
             });
     }).timeout(10000);
 });
